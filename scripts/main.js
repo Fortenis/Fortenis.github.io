64dff55974ff23632ff6f2be0f0b6a2c615c77c5
Fortenis <144365046+Fortenis@users.noreply.github.com>
2023-09-15 10:57:37 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,37 @@
-/*var myHeading = document.querySelector("h1");  Мы захватили функцией querySelector наш селектор с html и присвоили ему перменную под именнем myHeading, чтоб потом обращаться только к нему
-
-myHeading.textContent = "Hello world!"; Мы обратились к переменной и задали ей функцию которая меняет текст контет на указанный
-
-
-var iceCream = "chocolate";
-if (iceCream === "chocolate") {
-  alert("Yay, I love chocolate ice cream!");
-} else {
-  alert("Awwww, but chocolate is my favorite...");
-} /* это условие if ... else Оно проверяет если переменная равна if то выполнаяет одну функцию, если нет то выполняет функцию else*/
-
-
-function multiply(num1, num2) { //Мы обьявили свою функцию
-  var result = num1 * num2;     // указали действия функции
-  return result;                // Попросили возрващать результат функции
+function multiply(num1, num2) { 
+  var result = num1 * num2;     
+  return result;                
 }
 multiply(4, 7);
 multiply(20, 20);
-multiply(0.5, 3);   // проверяем функцию на работо спасобность (работает)
+multiply(0.5, 3);   
 
-
-/*document.querySelector("html").onclick = function () {
-  alert("Ouch! Stop poking me!");
-};  Мы присвоили документу html функцию котрая реагирует на клик по странице и вызывает окно*/
-
-
-var myImage = document.querySelector("img"); //мы присвоили селектор img в переменную myImage
-myImage.onclick = function () {  //Создаем фунцию при нажати на картинку
-  var mySrc = myImage.getAttribute("src"); // Создали новую переменную которая добовляет к нашей переменной функицю с изменением src
+var myImage = document.querySelector("img"); 
+myImage.onclick = function () {  
+  var mySrc = myImage.getAttribute("src"); 
   if (mySrc === "images/logo-igromaniya.png"){
-    myImage.setAttribute("src", "images/logo-igromaniya2.png"); //установили усливоие если картинка равна первоначальной то её нужно изменить на 2 картинку
+    myImage.setAttribute("src", "images/logo-igromaniya2.png"); 
   } else {
-  myImage.setAttribute("src", "images/logo-igromaniya.png");  // елси же  не ровна то она устанавливает на первоначальную картинку
+  myImage.setAttribute("src", "images/logo-igromaniya.png");  
 }
 };
 
 
-var myButton = document.querySelector("button"); // Создали переменную в которой заключили селектор кнопки на нашем сайте
-var myHeading = document.querySelector("h1");  // Создали пременную в которой заключили селектор нашего заголовка на нашем сайте
-function setUsername () {  //создали функцию  которая при входе просит вести сообщение
-  var myName = prompt("Пожалуйста представтесь."); //создали переменную с функцие всплывающего окна 
-  localStorage.setItem ("name", myName); //создали локальное хранилище, в котором будет храниться информация которую ввел посетитель
-  myHeading.textContent = "Игромания это плохо " + myName; //создали функцию котораю меняет наш заголовок на текст и берет информацию из хранилища
+var myButton = document.querySelector("button"); 
+var myHeading = document.querySelector("h1");  
+function setUsername () {  
+  var myName = prompt("Пожалуйста представтесь."); 
+  localStorage.setItem ("name", myName); 
+  myHeading.textContent = "Игромания это плохо " + myName; 
 }
 
-if (!localStorage.getItem("name")) { // создаем условие с оператора отрицания ! чтобы проверить есть ли информация в локальном хранилище под именем name. Если же информации нету, то выполнаяется функиция setUserName для их создания
+if (!localStorage.getItem("name")) { 
   setUsername();
 } else {
   var storedName = localStorage.getItem("name");
   myHeading.textContent = "Игромания это плохо " + storedName;
-} // Если же данные существует то он берет их и меняет наш заголовок на измененный текст.
+} 
 
 myButton.onclick = function() {
   setUsername();
-}; // Присвоили кнопке функцию которую создали ранее и по её нажатии можно будет менять свое имя
+}; 
